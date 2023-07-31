d11edcc2cbc2d466a71c464b7adb6a4b8fa2e3f1
dnsdesouza <dnsdesouza1@gmail.com>
2023-07-31 15:43:53 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline"  :class="{ 'modo-escuro':modoEscuroAtivo}">
+    <div class="column is-one-quarter">
+      <BarraLateral @aoTemaAlterado="trocarTema"/>
+    </div>
+    <div class=" column is-three-quarter conteudo">
+      <Formulario @aoSalvarTarefa="salvarTarefa" />
+      <div class="lista">
+        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+        <Box v-if ="listaEstaVazia">
+          Voce não está produtivo:(
+       </Box>
+     </div>
+     
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { computed, defineComponent } from 'vue';
+import BarraLateral from './components/BarraLareral.vue'
+import Formulario from './components/Formulario.vue'
+import Tarefa from './components/Tarefa.vue'
+import ITarefa from './interfaces/ITarefa'
+import Box from './components/Box.vue'
+// import Tarefa from './'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    BarraLateral,
+    Formulario,
+    Tarefa
+  },
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo:false
+    }
+  },
+  computed:{
+    listaEstaVazia() : boolean{
+      return this.tarefas.length === 0
+    }
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa)
+    },
+    trocarTema(modoEscuroAtivo: boolean){
+      this.modoEscuroAtivo = modoEscuroAtivo
+    }
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.lista {
+  padding: 1.25rem;
+}
+
+main{
+  --bg-primario:#d1dbe983;
+  --texto-primario:#000;
+  
+}
+main.modo-escuro{
+  --bg-primario:#252527;
+  --texto-primario:#f3f0f0;
+  
+}
+
+.conteudo{
+  background-color: var(--bg-primario);
 }
 </style>
---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="box ha-tex-weight-bold" :style="estilos">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { DefineComponent, defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Box',
+    data() {
+        return {
+            estilos: {
+                backgroundColor: '#FAF0CA'
+            }
+        }
+    }
+
+})
+</script>
